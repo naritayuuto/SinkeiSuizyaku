5b72b45b0f21159ae8131e8f9457b7222c54233f
naritayuuto <yuuto122narita@i.softbank.jp>
2024-03-24 16:41:52 +0900

    紛らわしい部分を関数で分けて分かりやすく簡潔にしました

--- Assets/Scripts/CordJudge.cs
+++ Assets/Scripts/CordJudge.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class CordJudge : MonoBehaviour,IPointerClickHandler
+public class CordJudge : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField,Header("デバッグ確認のため、セットしないこと")]
-    List<Cord> _cords = new List<Cord>();
     [SerializeField]
-    CordGenerater cordGenerater = null;
+    SceneState _sceneState = null;
+    [SerializeField]
+    CordGenerater _cordGenerater = null;
+    [SerializeField, Header("デバッグ確認のため、セットしないこと")]
+    List<Cord> _cords = new List<Cord>();
     [SerializeField, Header("clear確認用のパネル")]
     GameObject _panel = null;
     [Tooltip("要素数零番目を示す値")]
@@ -19,44 +21,30 @@ public class CordJudge : MonoBehaviour,IPointerClickHandler
     const int _maxPairNum = 2;
     [Tooltip("ペアかどうか判定中")]
     bool _judge = false;
+    [Tooltip("ペアが揃った場合True")]
+    bool _pair = false;
     // Start is called before the first frame update
     void Start()
     {
-        if(cordGenerater == null)
+        if (_cordGenerater == null)
         {
-            Debug.LogError($"cordGeneraterを{gameObject.name}のCordJudgeにセットしてください");
+            Debug.LogError($"CordGeneraterを{gameObject.name}のCordJudgeにセットしてください");
+        }
+        if (_sceneState == null)
+        {
+            Debug.LogError($"SceneStateを{gameObject.name}のCordJudgeにセットしてください");
         }
         _panel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (_judge == false)
+        if (_judge == false && _sceneState.TurnState == TurnState.player1 && _sceneState.StageState == StageState.erabu)
         {
             GameObject obj = eventData.pointerCurrentRaycast.gameObject;
             if (obj.TryGetComponent(out Cord cord))
             {
-                if (_cords.Count < _maxPairNum)
-                {
-                    cord.OpenAnim();
-                    _cords.Add(cord);
-                    if (_cords.Count == _maxPairNum)
-                    {
-                        if (_cords[_zero].CordData._numImage == _cords[_one].CordData._numImage)
-                        {
-                            _cords.Remove(_cords[_one]);
-                        }
-                        else
-                        {
-                            StartCoroutine(Judge());
-                        }
-                    }
-                }
+                CordOpen(cord);
             }
             else
             {
@@ -64,9 +52,35 @@ public class CordJudge : MonoBehaviour,IPointerClickHandler
             }
         }
     }
+
+    /// <summary>
+    /// カードをめくる
+    /// </summary>
+    /// <param name="cord"></param>
+    public void CordOpen(Cord cord)
+    {
+        if (_cords.Count < _maxPairNum)
+        {
+            cord.OpenAnim();
+            _cords.Add(cord);
+            if (_cords.Count == _maxPairNum)
+            {
+                if (_cords[_zero].CordData._numImage == _cords[_one].CordData._numImage)
+                {
+                    _cords.Remove(_cords[_one]);
+                }
+                else
+                {
+                    StartCoroutine(Judge());
+                }
+            }
+        }
+    }
     IEnumerator Judge()
     {
+        //確認開始
         _judge = true;
+        _sceneState.StageState = StageState.pairJudge;
         yield return new WaitForSeconds(_one);
         if (_cords[_zero].CordData._num == _cords[_one].CordData._num)
         {
@@ -74,6 +88,7 @@ public class CordJudge : MonoBehaviour,IPointerClickHandler
             {
                 cord.DisappearCord();
             }
+            _pair = true;
         }
         else
         {
@@ -81,20 +96,38 @@ public class CordJudge : MonoBehaviour,IPointerClickHandler
             {
                 cord.CloseAnim();
             }
+            _pair = false;
         }
         _cords = new List<Cord>();
         yield return new WaitForSeconds(_one);
         _judge = false;
         ClearJudge();
+        TurnJudge();
         yield break;
+    }
+
+    /// <summary>
+    /// ペアが揃っていたら同じ人に、そうでなければ次の人へ手番を移行する。
+    /// </summary>
+    public void TurnJudge()
+    {
+        _sceneState.StageState = StageState.ennsyutu;
+        if (!_pair)
+        {
+            _sceneState.TurnChange();
+        }
+        else
+        {//揃っていたら同じ人がめくる
+            _sceneState.TurnState = _sceneState.TurnState;
+        }
     }
 
     void ClearJudge()
     {
         bool clear = true;
-        foreach(var cord in cordGenerater.Cords)
+        foreach (var cord in _cordGenerater.Cords)
         {
-            if(cord.Disappear == false)
+            if (cord.Disappear == false)
             {
                 clear = false;
                 break;
